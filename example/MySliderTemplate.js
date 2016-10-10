e858a3007807c2c77558c039f22a06cb62e008da
Tyler Nielsen <tylerscottnielsen@gmail.com>
2016-10-10 13:57:30 -0500

    Added missing file.

---
+++ example/MySliderTemplate.js
@@ -0,0 +1,22 @@
+var React = require('react');
+
+var MySliderTemplate = React.createClass({
+	propTypes: {  //Since these props are introduced via createElement, making them required produces an error.
+		content: React.PropTypes.object,
+	},
+
+	render: function () {
+		var title = this.props.content.title;
+		var body = this.props.content.body;
+
+		return (
+			<div>
+				<span className="mySlider-title">{title}</span>
+				<br />
+				<span>{body}</span>
+			</div>
+		);
+	}
+});
+
+module.exports = MySliderTemplate;
